db69c611ccfe6de2d7c23d1e0ceb1160bcc38c1c
phpBB España <thekuka@gmail.com>
2016-02-27 21:36:05 +0100

    Spanish translation
    Spanish translation

---
+++ kasimi/logoutredirect/language/es/info_acp_logoutredirect.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Logout Redirect
+ * @copyright (c) 2016 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOGOUTREDIRECT_TITLE'				=> 'Redirección de Salida',
+	'LOGOUTREDIRECT_CONFIG'				=> 'Configuración',
+	'LOGOUTREDIRECT_CONFIG_UPDATED'		=> '<strong>Extensión Redirección de Salida</strong><br />» Configuración actualizada',
+	'LOGOUTREDIRECT_ENABLED'			=> 'Redirigir a los usuarios después de cerrar la sesión',
+	'LOGOUTREDIRECT_URL'				=> 'URL de redirección',
+	'LOGOUTREDIRECT_URL_EXPLAIN'		=> '',
+));
